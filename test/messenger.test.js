0b1fbb5562182397daebbe91c4c46568478914b7
Alexandre Silva <contato@silvalexandre.com>
2017-08-28 14:17:53 +0000

    adds sendWritting action; improves comments; changes test according to platform; minor improvements

--- test/messenger.test.js
+++ test/messenger.test.js
@@ -2,31 +2,59 @@
 	'use strict';
 	require('dotenv').load();
 	const expect = require('chai').expect;
-	const uid = 'someuid';
 	const messengerPlatform = require('../platform/messenger');
 	const Messenger = new messengerPlatform();
-	
+
+	const url = 'https://graph.facebook.com/v2.6/me/messages';
+	const access_token = 'EAAIgOCRb6bYBAAcWZCkgxP47gBnrHNO7HseKOE6FgIo4lqZCZChiJIWpBpGIkhpYFCUKCrVNEIA5SeTZB0UabwJF7SIuYIZCZAhyvcVtpQOOrFAOjAYZAdWjIfcql9FC5TrSwOZCiFVVvzYfO1Swe2iDrfomQIE3A3qUWOYpX0u5kQZDZD';
+	const method = 'POST';
+	const id = 'someuid';
+	const qs = {
+		access_token,
+	};
+	const recipient = {
+		id,
+	};
+	const text = 'Ola, tudo bem pessoal?';
+	const options = {
+		url,
+		qs,
+		method,
+		json: {
+			recipient,
+			message: {
+				text,
+			},
+		},
+	};
+
+
 	describe('platform messenger', () => {
-		describe('send text', () => {
+		describe('send a text', () => {
+			it('should be the same object', () => {
+				Messenger.setUid(id);
+				Messenger.sendText(text).then(r => {
+					expect(r).to.deep.equal(options);
+				}).catch(e => {
+					// It should not reach here
+					console.log(e);
+				});
+			});
+		});
+		describe('send a writting action', () => {
 			it('should be the same object', () => {
 				const text = 'Ola, tudo bem pessoal?';
 				const options = {
-					url: 'https://graph.facebook.com/v2.6/me/messages',
-					qs: {
-						access_token: 'EAAIgOCRb6bYBAAcWZCkgxP47gBnrHNO7HseKOE6FgIo4lqZCZChiJIWpBpGIkhpYFCUKCrVNEIA5SeTZB0UabwJF7SIuYIZCZAhyvcVtpQOOrFAOjAYZAdWjIfcql9FC5TrSwOZCiFVVvzYfO1Swe2iDrfomQIE3A3qUWOYpX0u5kQZDZD'
-					},
-					method: 'POST',
+					url,
+					qs,
+					method,
 					json: {
-						recipient: {
-							id: uid
-						},
-						message: {
-							text: text
-						}
+						recipient,
+						sender_action: 'typing_on',
 					}
 				};
-				Messenger.setUid(uid);
-				Messenger.sendText(text).then(r => {
+				Messenger.setUid(id);
+				Messenger.sendWritting().then(r => {
 					expect(r).to.deep.equal(options);
 				}).catch(e => {
 					// It should not reach here
