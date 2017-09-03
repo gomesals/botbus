b21d6c0b5bd00ab08e31a361dcafda43a60c6f0e
Alexandre Silva <contato@silvalexandre.com>
2017-09-03 19:21:57 +0000

    adds searching terms and related funcionalities

--- platform/general.js
+++ platform/general.js
@@ -13,6 +13,21 @@
         compliment: config.get('defaults.compliment'),
         confused: config.get('defaults.confused'),
     };
+    const searching = {
+        defaults: config.get('search.defaults'),
+        replace: {
+            verb: config.get('search.replace.verb'),
+            object: config.get('search.replace.object'),
+            before: config.get('search.replace.before'),
+            after: config.get('search.replace.after'),
+            icons: config.get('search.replace.icons'),
+        },
+    };
+
+    /**
+     * Class to handle the messages sent by the user.
+     * 
+     */
     class General {
         /**
          * Sets the platform.
@@ -59,10 +74,10 @@
          */
         treat() {
             const { intent, ...search } = Analyze.content(this.text);
-            console.log(intent);
-            console.log(search);
+            // console.log(intent);
             if (intent.isAllowed) {
-                this.platform.sendText('pesquisando...');
+                this.platform.sendText(getSearchingText(search));
+                console.log(search);
                 // TODO: search and send the information
                 return;
             }
@@ -102,5 +117,30 @@
      */
     function getRandom(length) {
         return Math.floor(Math.random() * length);
+    }
+    /**
+     * Returns a random text term for searching results.
+     * 
+     * @param {Object} search options to be used at replace.
+     * @return  {String} The text to be sent.
+     * 
+     */
+    function getSearchingText(search) {
+        const message = {
+            text: searching.defaults[getRandom(searching.defaults.length)],
+            verb: searching.replace.verb[getRandom(searching.replace.verb.length)],
+            object: searching.replace.object[getRandom(searching.replace.object.length)],
+            before: searching.replace.before[getRandom(searching.replace.before.length)].replace('{time}', search.timeShow),
+            after: searching.replace.after[getRandom(searching.replace.after.length)].replace('{time}', search.timeShow),
+            icons: searching.replace.icons[getRandom(searching.replace.icons.length)],
+        };
+        const schedule = search.check === 'gte' ? message.after : message.before;
+        message.text = message.text.replace('{verb}', message.verb);
+        message.text = message.text.replace('{object}', message.object);
+        message.text = message.text.replace('{from}', search.from);
+        message.text = message.text.replace('{to}', search.to);
+        message.text = message.text.replace('{schedule}', schedule);
+        message.text += ` ${message.icons}`;
+        return message.text;
     }
 })();
